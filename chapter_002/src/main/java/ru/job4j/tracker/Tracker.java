ad344862581b2382ec0bb21d9fc7ce8e01b606b8
litvinova08 <ma.litvinova08@gmail.com>
2022-03-02 16:19:15 +1100

    8. Что такое валидация? [#246864 #270630] V2

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,10 +59,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            items[index] = null;
-            int start = index + 1;
-            int length = size - index;
-            System.arraycopy(items, start, items, index, length);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
