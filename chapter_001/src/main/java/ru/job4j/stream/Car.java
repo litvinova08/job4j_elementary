f48f2c6f94445eb67e92c0af73f8fd8f6c4c336c
litvinova08 <ma.litvinova08@gmail.com>
2022-03-21 16:18:42 +1100

    9. Шаблоны проектирования в Stream API [#310012]

---
+++ chapter_001/src/main/java/ru/job4j/stream/Car.java
@@ -0,0 +1,89 @@
+package ru.job4j.stream;
+
+import java.time.LocalDate;
+
+public class Car {
+    private String brand;
+
+    private String model;
+
+    private LocalDate created;
+
+    private double volume;
+
+    private String color;
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "brand='" + brand + '\''
+                + ", model='" + model + '\''
+                + ", created=" + created
+                + ", volume=" + volume
+                + ", color='" + color + '\''
+                + '}';
+    }
+
+    static class Builder {
+        private String brand;
+        private String model;
+        private LocalDate created;
+        private double volume;
+        private String color;
+
+        Builder buildBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        Builder buildModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        Builder buildCreated(LocalDate created) {
+            this.created = created;
+            return this;
+        }
+
+        Builder buildVolume(double volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        Builder buildColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        Car build() {
+            Car car = new Car();
+            car.brand = brand;
+            car.color = color;
+            car.model = model;
+            car.created = created;
+            car.volume = volume;
+            return car;
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Builder()
+                .buildBrand("Toyota")
+                .buildModel("Camry")
+                .buildCreated(LocalDate.of(2021, 6, 1))
+                .buildVolume(2.5)
+                .buildColor("Red")
+                .build();
+        System.out.println(car);
+        Car campervan = new Builder()
+                .buildBrand("Trakka")
+                .buildColor("White")
+                .buildModel("RV")
+                .buildVolume(6.5)
+                .buildCreated(LocalDate.of(2022, 3, 21))
+                .build();
+        System.out.println(campervan);
+
+    }
+}
